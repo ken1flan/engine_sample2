0498ed0ddc650b236ba7fc4992de418f40f08d2a
ken1flan <ken1flan@gmail.com>
2023-05-19 08:22:47 +0900

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+# ユーザ
+csv_path = Rails.root.join('db', 'seeds', 'users.csv')
+CSV.foreach(csv_path, headers: true) do |row|
+  User.create!(
+    name: row['氏名'],
+  )
+end
+
+# プロジェクト
+csv_path = Rails.root.join('db', 'seeds', 'projects.csv')
+CSV.foreach(csv_path, headers: true).with_index do |row, index|
+  Project.create!(
+    title: row['プロジェクト名'],
+    body: row['概要'],
+    user: User.find(index + 1)
+  )
+end
+
+# ページ
+csv_path = Rails.root.join('db', 'seeds', 'pages.csv')
+CSV.foreach(csv_path, headers: true).with_index do |row, index|
+  Page.create!(
+    title: row['タイトル'],
+    body: row['本文'],
+    user: User.find(index + 1)
+  )
+end
