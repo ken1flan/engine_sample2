d4f1bb2b72ef1bf39d47584ec4c84f0e8d13730d
ken1flan <ken1flan@gmail.com>
2023-05-18 14:31:46 +0900

    Add assets to precompile

--- special/lib/special/engine.rb
+++ special/lib/special/engine.rb
@@ -1,5 +1,12 @@
 module Special
   class Engine < ::Rails::Engine
     isolate_namespace Special
+
+    initializer "special.assets.precompile", group: :all do |app|
+      app.config.assets.precompile += %w(
+        special/application.js
+        special/application.css
+      )
+    end
   end
 end
