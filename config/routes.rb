a7dbf36351b7e75e5d2e4dec2a7c488e6f7fcdbb
ken1flan <ken1flan@gmail.com>
2023-05-18 17:07:14 +0900

    Fix navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :pages
   root 'top#show'
   resources :users
   resources :projects
+  resources :pages
 
   mount Special::Engine => 'special'
 end
--- special/test/dummy/config/routes.rb
+++ special/test/dummy/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'projects#index'
   resources :projects
   resources :users
+  resources :pages
 
   mount Special::Engine => "/special"
 end
